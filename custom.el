5c723a68695a38d620e1be6dce534aaded4f5553
Michael Gregson <mgregson@gregson.io>
2018-09-06 23:18:41 -0700

    purescript packages

--- custom.el
+++ custom.el
@@ -39,7 +39,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (use-package intero nix-mode vue-mode js2-mode golint flymake-go go-tag go-add-tags go-imports go-dlv company-go go-mode flymake-solidity solidity-mode cider php-mode markdown-mode+ el-get json-mode yaml-mode dired-toggle-sudo dired-toggle flymake-easy flycheck flyspell-lazy gitignore-mode gitconfig-mode gist magit company css-eldoc scss-mode sass-mode csharp-mode company-inf-ruby nginx-mode erc-hl-nicks csv-mode ess flymake-haskell-multi flycheck-haskell company-ghc ghci-completion ghc haskell-mode protobuf-mode thrift company-c-headers c-eldoc sbt-mode scala-mode dropdown-list bbdb)))
+    (psc-ide-emacs psci flycheck-purescript purescript-mode psc-ide use-package intero nix-mode vue-mode js2-mode golint flymake-go go-tag go-add-tags go-imports go-dlv company-go go-mode flymake-solidity solidity-mode cider php-mode markdown-mode+ el-get json-mode yaml-mode dired-toggle-sudo dired-toggle flymake-easy flycheck flyspell-lazy gitignore-mode gitconfig-mode gist magit company css-eldoc scss-mode sass-mode csharp-mode company-inf-ruby nginx-mode erc-hl-nicks csv-mode ess flymake-haskell-multi flycheck-haskell company-ghc ghci-completion ghc haskell-mode protobuf-mode thrift company-c-headers c-eldoc sbt-mode scala-mode dropdown-list bbdb)))
  '(password-cache-expiry 300)
  '(running-xemacs f)
  '(safe-local-variable-values
--- packages.el
+++ packages.el
@@ -78,7 +78,11 @@
 
 (defvar mg/packages-javascript
   '(js2-mode
-    vue-mode))
+    vue-mode
+    psc-ide
+    psci
+    purescript-mode
+    flycheck-purescript))
 
 (defvar mg/packages-core
   '(bbdb
